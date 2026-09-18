c4b8141dcf9cf758d5ca85d1d2f50d43be2740da
dmitry.shepeta <shepeta@rtt.com>
2026-09-18 04:30:33 +0300

    Activity double id fix

---
+++ android/app/src/main/java/com/legbehindneck/wudict/LookupActivity_sh.java
@@ -0,0 +1,8 @@
+// Copyright (C) 2026 glowinthedark
+// SPDX-License-Identifier: GPL-3.0-or-later
+package com.legbehindneck.wudict;
+
+/** Distinct component name for callers that identify dictionaries by Activity class. */
+public class LookupActivity_sh extends LookupActivity {
+    // Share the lookup implementation; only this component is registered in sh builds.
+}
